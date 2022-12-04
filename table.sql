10fb013131711481b4cdf8385c385679c5ec1453
Valentin Shepitko <loungestudiopo@gmail.com>
2022-12-03 21:48:40 -0500

    1

---
+++ table.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE tableName 
+(
+    status	varchar(300),
+    name	varchar(300),
+    value	varchar(300),
+    id	varchar(300),
+    link	varchar(300)
+);
+
+INSERT INTO tableName (status, name, value, id, link) VALUES ('To Do', 'Blanket Concern Process', 'SWAD_APIM_2023U553N/4N', 'ATM-247', 'https://ford-dev-alcatraz.atlassian.net/browse/ATM-247');
+INSERT INTO tableName (status, name, value, id, link) VALUES ('To Do', 'Upload', 'SWAD_APIM_2023U553N/4N', 'ATM-248', 'https://ford-dev-alcatraz.atlassian.net/browse/ATM-248');
+INSERT INTO tableName (status, name, value, id, link) VALUES ('To Do', 'Testing Status', 'SWAD_APIM_2023U553N/4N', 'ATM-249', 'https://ford-dev-alcatraz.atlassian.net/browse/ATM-249');
+INSERT INTO tableName (status, name, value, id, link) VALUES ('To Do', 'Update HC', 'SWAD_APIM_2023U553N/4N', 'ATM-250', 'https://ford-dev-alcatraz.atlassian.net/browse/ATM-250');
+INSERT INTO tableName (status, name, value, id, link) VALUES ('To Do', 'FORCE', 'SWAD_APIM_2023U553N/4N', 'ATM-251', 'https://ford-dev-alcatraz.atlassian.net/browse/ATM-251');
+INSERT INTO tableName (status, name, value, id, link) VALUES ('To Do', 'VADR', 'SWAD_APIM_2023U553N/4N', 'ATM-252', 'https://ford-dev-alcatraz.atlassian.net/browse/ATM-252');
+INSERT INTO tableName (status, name, value, id, link) VALUES ('To Do', 'Ice Packs', 'SWAD_APIM_2023U553N/4N', 'ATM-253', 'https://ford-dev-alcatraz.atlassian.net/browse/ATM-253');
+INSERT INTO tableName (status, name, value, id, link) VALUES ('To Do', 'Release Notes', 'SWAD_APIM_2023U553N/4N', 'ATM-254', 'https://ford-dev-alcatraz.atlassian.net/browse/ATM-254');
+INSERT INTO tableName (status, name, value, id, link) VALUES ('In Progress', '', 'SWAD_TCU_2023CX727N', 'ATM-218', 'https://ford-dev-alcatraz.atlassian.net/browse/ATM-218');
+INSERT INTO tableName (status, name, value, id, link) VALUES ('In Progress', '', 'SWAD_ECG_2023.5CX727N', 'ATM-231', 'https://ford-dev-alcatraz.atlassian.net/browse/ATM-231');
+INSERT INTO tableName (status, name, value, id, link) VALUES ('In Progress', 'SUD', 'SWAD_APIM_2023U553N/4N', 'ATM-246', 'https://ford-dev-alcatraz.atlassian.net/browse/ATM-246');
